ec38811fe879d6f7b8494e39e8b9e06870f67372
fred-dev <fredaudio@gmail.com>
2023-02-09 15:05:03 +1100

    Added python script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,7 @@ module.exports = {
 	apps : [{
 		name		:"Gaugemancy",
 		script	:"cd /home/raspberry/openFrameworks/apps/myApps/Gaugemancy/bin &&./Gaugemancy",
+		autorestart: false,
 		exec_interpreter: "none",
 		exec_mode : "fork_mode"
 	}]
