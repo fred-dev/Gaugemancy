4d07637798a2832a34c9a06aa9571d3e71c1e365
Frederick Rodrigues <fredaudio@gmail.com>
2023-02-09 15:06:50 +1100

    Try python start first

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-	apps : [{
-		name		:"Gaugemancy",
-		script	:"cd /home/raspberry/openFrameworks/apps/myApps/Gaugemancy/bin &&./Gaugemancy",
-		autorestart: false,
-		exec_interpreter: "none",
-		exec_mode : "fork_mode"
-	}]
+  apps : [{
+    name: 'Prep_GPIO',
+    script: '/home/raspberry/openFrameworks/apps/myApps/Gaugemancy/prep_GPIO.py',
+    interpreter: 'python3',
+    autorestart: false,
+  },
+  {
+    name: "Gaugemancy",
+    script: "cd /home/raspberry/openFrameworks/apps/myApps/Gaugemancy/bin &&./Gaugemancy",
+    autorestart: false,
+    exec_interpreter: "none",
+    exec_mode : "fork_mode",
+    wait_boot: 3000,
+  }]
 }
